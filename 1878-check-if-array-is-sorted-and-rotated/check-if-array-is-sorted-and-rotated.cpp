f9cf7550a058de15e5696f0c4bec21f3d704b862
Sreeja <deysasha262@gmail.com>
2026-07-01 20:17:16 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (54.80%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -10,11 +10,15 @@ public:
             }
         }
 
-        if(nums[n-1] > nums[0]) {
+        if(nums[n-1] > nums[0]) { //compare last and first element
             count++;
         }
 
-        if(count == 0 || count == 1) {
+
+        //if count is 0, it means array contains same elements.
+        //if count is 1, it means array is sorted and rotated. 
+        //if count > 1, it means array is unsorted.
+        if(count == 0 || count == 1) { 
             return true;
         }
         else {
