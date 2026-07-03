a25e7e113518268ba219719ac1ef22442d850702
Sreeja <deysasha262@gmail.com>
2026-07-03 11:56:14 +0530

    Time: 3 ms (22.96%) | Memory: 20.6 MB (58.34%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,17 +1,30 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
+        int xor1 = 0;
+        for(int i=0; i<nums.size(); i++) {
+            xor1 = xor1 ^ nums[i];
+        }
+        return xor1;
+
+
+
+
+
+        /*
+        //TC=O(N) + O(n/2 +1), SC=O(N/2 +1)
         unordered_map<int, int> m;
 
-        for(int i=0; i< nums.size(); i++) {
+        for(int i=0; i< nums.size(); i++) { //this will run n times
             m[nums[i]]++;
         }
 
-        for(auto i : m) {
+        for(auto i : m) { //This loop will run for (n/2 +1) times
             if(i.second == 1) {
                 return i.first;
             }
         }
         return -1;
+        */
     }
 };
