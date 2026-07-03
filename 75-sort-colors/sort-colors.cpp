800cbcc260766bc1ff8b9d8410749dd98393bc5d
Sreeja <deysasha262@gmail.com>
2026-07-03 23:54:24 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (48.63%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,9 +1,33 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
+        //Optimal solution: Dutch National Flag Algorithm: 3 pointers
+        //TC=O(), SC=O(1)
+        int n = nums.size();
+        int low = 0, mid = 0, high = n-1;
+
+        while(mid <= high) {
+            if(nums[mid] == 0) {
+                swap(nums[low], nums[mid]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid] == 1){
+                mid++;
+            }
+            else{
+                swap(nums[high],nums[mid]);
+                high--;
+            }
+        }
+
+
+
+
+
         //Better solution
         //TC = O(2N), SC=O(1)
-        int n = nums.size();
+        /*int n = nums.size();
         int count_0 = 0, count_1 = 0, count_2 = 0;
 
         for(int i=0; i<n; i++) { //this loop runs N times
@@ -26,6 +50,6 @@ public:
         }
         for(int i=count_0 + count_1; i<n; i++) {
             nums[i] = 2;
-        }
+        } */
     }
 };
