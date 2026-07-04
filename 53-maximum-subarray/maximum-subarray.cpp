51c871bd43bead2ca1ca2dc65a03dcec6ece1343
Sreeja <deysasha262@gmail.com>
2026-07-04 13:30:07 +0530

    Time: 0 ms (100.00%) | Memory: 71.9 MB (14.51%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -32,15 +32,20 @@ public:
         return maxSum;*/
 
 
+
         //Optimal Solution: Kadane's Algo
-        int currSum = 0;
+        //TC=O(N), SC=0(1)
+        int n = nums.size();
+        int sum = 0;
         int maxSum = INT_MIN;
 
-        for(int i : nums){
-            currSum += i;
-            maxSum = max(currSum, maxSum);
-            if(currSum < 0){
-                currSum = 0;
+        for(int i=0; i<n; i++){
+            sum += nums[i];
+
+            maxSum = max(sum, maxSum);
+
+            if(sum < 0){
+                sum = 0;
             }
         }
         return maxSum;
