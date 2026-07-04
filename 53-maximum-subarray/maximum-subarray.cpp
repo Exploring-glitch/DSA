43c14bb51dd86d7d8a4b5782599e667ca7336557
Sreeja <deysasha262@gmail.com>
2026-07-04 13:07:47 +0530

    Time: 3 ms (19.20%) | Memory: 71.6 MB (98.62%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,6 +1,38 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
+        //Brute Force Appproach: take out all the possible subarrays and check the max sum
+        //TC=O(N^3), SC=O(1)
+        /*int n = nums.size();
+        int maxSum = INT_MIN;
+        for(int i=0; i<n; i++) {
+            for(int j=i; j<n; j++) {
+                int sum = 0;
+                for(int x=i; x<=j; x++) {
+                    sum += nums[x];
+                }
+                maxSum = max(sum, maxSum);
+            }
+        }
+        return maxSum;*/
+
+
+
+        //Better Approach: Same approach as before, just optimize the inner loop 
+        //TC=O(N^2), SC=O(1)
+        /*int n = nums.size();
+        int maxSum = INT_MIN;
+        for(int i=0; i<n; i++) {
+            int sum = 0;
+            for(int j=i; j<n; j++) {
+                sum += nums[j];
+                maxSum = max(sum, maxSum);
+            }
+        }
+        return maxSum;*/
+
+
+        //Optimal Solution: Kadane's Algo
         int currSum = 0;
         int maxSum = INT_MIN;
 
@@ -11,7 +43,7 @@ public:
                 currSum = 0;
             }
         }
-        return {maxSum};
+        return maxSum;
 
     }
 };
