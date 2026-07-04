10e54891a28f3acd3dc8bd93abd9ef2bd5c8c53a
Sreeja <deysasha262@gmail.com>
2026-07-04 11:51:40 +0530

    Time: 0 ms (100.00%) | Memory: 28.3 MB (31.43%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,7 +1,28 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        //Better Approach: Hashinh
+        //TC=O(), SC=O()
+
         int n = nums.size();
+        unordered_map<int,int> mpp;
+        
+        for(int i = 0; i<n; i++){
+            mpp[nums[i]]++;
+        }
+
+        for(auto i : mpp){
+            if(i.second > n/2) {
+                int ans = i.first;
+                return ans;
+            }
+        }
+        return{};
+
+
+        //Brute Force Approach
+        //TC=O(N^2), SC=O(1)
+        /*int n = nums.size();
         for(int i : nums){
             int freq = 0;
             for(int ele : nums){
@@ -10,10 +31,10 @@ public:
                 }
             }
 
-            if ( freq > n/2){
+            if (freq > n/2){
                 return{i};
             }
         }
-        return{};
+        return{};*/
     }
 };
