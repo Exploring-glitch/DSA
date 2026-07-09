d51873521a70dc560c3dc8caba0d9dcec1122445
Sreeja <deysasha262@gmail.com>
2026-07-09 15:26:11 +0530

    Time: 55 ms (38.51%) | Memory: 51 MB (20.50%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,8 +1,26 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        //Brute Force Approach
+        //Optimal Approach
+        //TC=O(N) , SC=O(N)
+        int n = nums.size();
+        int count = 0, preSum = 0;
+        unordered_map<int, int> mpp;
+        mpp[0] = 1;
+
+        for(int i=0; i<n; i++) { //runs N times
+            preSum += nums[i];
+            int remaining = preSum - k;
+            count += mpp[remaining];
+            mpp[preSum] += 1;
+        }
+        return count;
+
+
+
+        //Brute Force Approach (Better)
         //TC=O(N^2) , SC=O(1)
+        /*
         int n = nums.size();
         int count = 0;
 
@@ -10,13 +28,13 @@ public:
             int s = 0;
             for(int j=i; j<n; j++) {
                 s += nums[j];
-                
+
                 if(s == k) {
                     count++;
                 }
             }
         }
-
         return count;
+        */
     }
 };
