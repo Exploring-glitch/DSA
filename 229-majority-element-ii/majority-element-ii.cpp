9608cf4b99a37c895800b50cb277900ea862ee5d
Sreeja <deysasha262@gmail.com>
2026-07-10 21:23:13 +0530

    Time: 919 ms (5.15%) | Memory: 23.6 MB (64.81%) - LeetSync

---
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        //Brute Force Approach
+
+        vector<int> ans;
+        int n = nums.size();
+
+        for(int i=0; i<n; i++) {
+            if(ans.size() == 0 || find(ans.begin(), ans.end(), nums[i]) == ans.end()) {
+                int c=0;
+                for(int j=0; j<n; j++) {
+                    if(nums[i] == nums[j]) {
+                        c++;
+                    }
+                }
+                if(c > (n/3)) {
+                     ans.push_back(nums[i]);   
+                }
+            }
+        }
+
+        return ans;
+    }
+};
