cb7297a8d50443344481e2103d0e620f8fac5429
Sreeja <deysasha262@gmail.com>
2026-07-11 21:55:56 +0530

    Time: 4 ms (65.93%) | Memory: 23.7 MB (95.47%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,9 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        //Optimal Solution
+        int n = intervals.size();
+        vector<vector<int>> ans;
+        sort(intervals.begin(), intervals.end()); //takes N logN time
+
+        for(int i=0; i<n; i++) { 
+            if(ans.empty() || intervals[i][0] > ans.back()[1]){
+                ans.push_back(intervals[i]);
+            }
+            else {
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+            }
+        }
+
+        return ans; 
+
+
+
         //Brute Force Apprpoach
         //TC=O(N logN + 2N), SC=O(N)
-
+        /*
         int n = intervals.size();
         vector<vector<int>> ans;
         sort(intervals.begin(), intervals.end()); //takes N logN time
@@ -26,5 +44,6 @@ public:
         }
 
         return ans;
+        */
     }
 };
