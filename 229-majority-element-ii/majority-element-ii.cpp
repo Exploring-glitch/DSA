76691c8db8b84b4ea3c77eba4117638d424869df
Sreeja <deysasha262@gmail.com>
2026-07-11 13:10:34 +0530

    Time: 6 ms (41.68%) | Memory: 23.8 MB (44.91%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,8 +1,43 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        //Optimal Approach: using hashmap
+        //Optimal Solution
+        //TC=O(2N), SC=O(1)
+        int n = nums.size();
+        vector<int> ls; 
+        int mini = (int)(n / 3) + 1;
+        int c1 = 0, c2 = 0;
+        int ele1 = INT_MIN, ele2 = INT_MIN;
+        int count1 = 0, count2 = 0;
+
+        for(int i=0; i<n; i++) {
+            if(c1 == 0 && nums[i] != ele2) {
+                c1 = 1;
+                ele1 = nums[i];
+            }
+            else if(c2 == 0 && nums[i] != ele1) {
+                c2 = 1;
+                ele2 = nums[i];
+            }
+            else if (nums[i] == ele1) c1++;
+            else if (nums[i] == ele2) c2++;
+            else c1--, c2--;
+        }
+
+        for (int i=0; i<n; i++) {
+            if (nums[i] == ele1) count1++;
+            if (nums[i] == ele2) count2++;
+        }
+        if(count1 >= mini) ls.push_back(ele1);
+        if(count2 >= mini) ls.push_back(ele2);
+        sort(ls.begin(), ls.end());
+        return ls;
+        
+
+
+        //Better Approach: using hashmap
         //TC=O(2N), SC=O(N)
+        /*
         unordered_map<int, int> mpp;
         vector<int> ans;
         int n = nums.size();
@@ -21,6 +56,8 @@ public:
         }
         sort(ans.begin(), ans.end());
         return ans;
+        */
+
 
 
         //Brute Force Approach
