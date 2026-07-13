f825fc1cd3dd0af1b9e2393bbb7f014e2f77bf76
Sreeja <deysasha262@gmail.com>
2026-07-13 20:18:13 +0530

    Time: 0 ms (100.00%) | Memory: 31.4 MB (46.38%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
+    int recursiveFunc(vector<int>& nums, int low, int high, int target) {
+        if (low > high) {
+            return -1;
+        }
+
+        int mid = (low + high) / 2;
+        if(target == nums[mid]) {
+                return mid;
+            }
+            else if(target > nums[mid]) {
+                return recursiveFunc(nums, mid + 1, high, target);            
+            }
+            else {
+                return recursiveFunc(nums, low, mid - 1, target); 
+            }
+
+    }
     int search(vector<int>& nums, int target) {
+        //Recursive approach
+        int n = nums.size();
+        int low = 0;
+        int high = n-1;
+        return recursiveFunc(nums, low, high, target);
+
+        //Bineray Search Algo
+        //TC=O(LOGN), SC=O(1)
+        /*
         int n = nums.size();
         int low = 0;
         int high = n-1;
@@ -11,13 +37,14 @@ public:
             if(target == nums[mid]) {
                 return mid;
             }
-            
-            if(target > nums[mid]) {
-                low = mid + 1;            }
+            else if(target > nums[mid]) {
+                low = mid + 1;            
+            }
             else {
                 high = mid - 1;
             }
         }
         return -1;
+        */
     }
 };
