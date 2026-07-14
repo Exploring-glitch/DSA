de56cf3460757ab17ac65f7f73e14ad3b086bce3
Sreeja <deysasha262@gmail.com>
2026-07-14 21:13:52 +0530

    Time: 0 ms (100.00%) | Memory: 26.2 MB (35.00%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,8 +1,37 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
+        //Optimal Solution
+        //TC=O(LOGN), SC=O(1)
+        int n = nums.size();
+
+        if(n == 1) return nums[0];
+        if(nums[0] != nums[1]) return nums[0];
+        if(nums[n-1] != nums[n-2]) return nums[n-1];
+        
+        int low = 1, high = n-2;
+
+        while( low <= high) {
+            int mid = (low+high)/2;
+
+            if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]) return nums[mid];
+
+            if(mid % 2 == 1 && nums[mid] == nums[mid-1] || mid % 2 == 0 && nums[mid] == nums[mid+1]) { //element is on the right half so eliminate the left half
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            } 
+        }
+        return -1;
+
+
+
+
+
         //Brute force solution
         //TC=O(N), SC=O(1)
+        /*
         int n = nums.size();
         if(n == 1) return nums[0];
 
@@ -18,5 +47,6 @@ public:
             }
         }
         return -1;
+        */
     }
 };
