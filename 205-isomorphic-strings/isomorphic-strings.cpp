85fb61090c172bb423b7297a3c6ab43b04ded956
Sreeja <deysasha262@gmail.com>
2026-08-27 12:30:52 +0530

    Time: 1334 ms (6.37%) | Memory: 9 MB (98.86%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        //TC=O(N) where N is the length of an input string
-        // SC=O(1)
-        int arr1[256] = {0};
+        //Optimal Solution:
+        //TC=O(N) where N is the length of an input string, SC=O(1)
+        /*int arr1[256] = {0};
         int arr2[256] = {0};
 
         for(int i=0; i<s.length(); i++) {
@@ -12,6 +12,16 @@ public:
             }
             arr1[s[i]] = i+1;
             arr2[t[i]] = i+1;
+        }
+        return true;*/
+
+        //Brute Force Solution:
+        //TC=O(N^2), SC=O(1)
+        for(int i = 0; i < s.length(); i++) {
+            for(int j = i+1; j < s.length(); j++) {
+                if(s[i] == s[j] && t[i] != t[j]) return false;
+                if(t[i] == t[j] && s[i] != s[j]) return false;
+            }
         }
         return true;
     }
