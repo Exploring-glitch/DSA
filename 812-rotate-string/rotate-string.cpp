9cb2fd6a7f0096d772b288ea87ce535a3886e66c
Sreeja <deysasha262@gmail.com>
2026-08-28 10:47:34 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (83.07%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -8,7 +8,7 @@ public:
 
         for(int i=0; i<s.length(); i++) { //O(n) times
             char ch = s[0];
-            s.erase(0, 1);
+            s.erase(0, 1); //remove 1 character in 0th position
             s.push_back(ch);
             if(s == goal) return true; //O(n) times
         }
