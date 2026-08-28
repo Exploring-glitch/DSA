3ff0373e054045471e76e1babb561679bd356ccf
Sreeja <deysasha262@gmail.com>
2026-08-28 10:36:38 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (55.90%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
+        //Brute Force Solution
+        //TC=O(N^2), SC=O(1)
         if(s.length() != goal.length()) return false;
+        if(s == goal) return true;
 
-        for(int i=0; i<s.length(); i++) {
+        for(int i=0; i<s.length(); i++) { //O(n) times
             char ch = s[0];
             s.erase(0, 1);
             s.push_back(ch);
-            if(s == goal) return true;
+            if(s == goal) return true; //O(n) times
         }
         return false;
     }
