b417a9f41fca9cf9fd0e783af3a8c54787dca190
Sreeja <deysasha262@gmail.com>
2026-08-31 19:56:07 +0530

    Time: 10 ms (7.97%) | Memory: 17.3 MB (6.04%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        //Brute fOrce Solution: TC=O(N^2 + 2N), SC=O(2K) where K is the distinct ch in s
-        unordered_map<char, int> mp; //K space
-        for(auto ch : s) {
-            mp[ch]++;
+        
+        unordered_map<char, int> freq;
+
+        // Count frequency
+        for(char ch : s) {
+            freq[ch]++;
         }
 
-        vector<pair<char, int>> v(mp.begin(), mp.end()); //store the map mp into vector v //K space
+        // Bucket: index = frequency
+        vector<vector<char>> bucket(s.size() + 1);
 
-        for(int i = 0; i < v.size(); i++) { //sort v by frequency //N^2
-            for(int j = i + 1; j < v.size(); j++) {
-                if(v[j].second > v[i].second) {
-                    swap(v[i], v[j]);
-                }
-            }
+        for(auto p : freq) {
+            bucket[p.second].push_back(p.first);
         }
 
+        // Build answer from highest frequency
         string ans = "";
-        for(auto i : v) { //N
-            ans += string(i.second, i.first);
+
+        for(int i = s.size(); i >= 1; i--) {
+            for(char ch : bucket[i]) {
+                ans += string(i, ch);
+            }
         }
 
         return ans;
     }
 };
+        
