1dfd0f4ed523e4688669c1df4e9a6c97dab07f6e
Sreeja <deysasha262@gmail.com>
2026-08-31 19:58:08 +0530

    Time: 8 ms (12.03%) | Memory: 17.2 MB (6.04%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -26,6 +26,31 @@ public:
         }
 
         return ans;
+
+        /*
+        //Brute force Solution: TC=O(N^2 + 2N), SC=O(2K) where K is the distinct ch in s
+        unordered_map<char, int> mp; //K space
+        for(auto ch : s) {
+            mp[ch]++;
+        }
+
+        vector<pair<char, int>> v(mp.begin(), mp.end()); //store the map mp into vector v //K space
+
+        for(int i = 0; i < v.size(); i++) { //sort v by frequency //N^2
+            for(int j = i + 1; j < v.size(); j++) {
+                if(v[j].second > v[i].second) {
+                    swap(v[i], v[j]);
+                }
+            }
+        }
+
+        string ans = "";
+        for(auto i : v) { //N
+            ans += string(i.second, i.first);
+        }
+
+        return ans;
+        */
     }
 };
         
