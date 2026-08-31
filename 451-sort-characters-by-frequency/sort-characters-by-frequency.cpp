0aec3884784e74469821312325eaafbc444dffb1
Sreeja <deysasha262@gmail.com>
2026-08-31 19:39:15 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (20.32%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        //Brute fOrce Solution: TC=O(N^2 + N), SC=O()
-        unordered_map<char, int> mp; //
+        //Brute fOrce Solution: TC=O(N^2 + 2N), SC=O(2K) where K is the distinct ch in s
+        unordered_map<char, int> mp; //K space
         for(auto ch : s) {
             mp[ch]++;
         }
 
-        vector<pair<char, int>> v(mp.begin(), mp.end()); //store the map mp into vector v 
+        vector<pair<char, int>> v(mp.begin(), mp.end()); //store the map mp into vector v //K space
 
         for(int i = 0; i < v.size(); i++) { //sort v by frequency //N^2
             for(int j = i + 1; j < v.size(); j++) {
