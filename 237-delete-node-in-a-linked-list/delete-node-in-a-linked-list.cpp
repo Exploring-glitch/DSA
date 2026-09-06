fb6d2308bb51bdfaee798f8f87bc9cea490cc6d8
Sreeja <deysasha262@gmail.com>
2026-09-06 23:25:04 +0530

    Time: 10 ms (34.93%) | Memory: 12.3 MB (56.60%) - LeetSync

--- 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
@@ -9,7 +9,8 @@
 class Solution {
 public:
     void deleteNode(ListNode* node) {
-        node->val = node->next->val;
-        node->next = node->next->next;
+        //TC=O(1), SC=O(1)
+        node->val = node->next->val; //copy the value of next node to the existing node
+        node->next = node->next->next; //skip the duplicate node
     }
 };
